0d46fcc0969f6200821719f968b6f8bc88a9593e
mkpoli <git@mkpo.li>
2024-09-28 22:43:22 +0900

    feat: add basic game UI

---
+++ src/lib/Game.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	let word = $state('');
+	let words: string[] = $state([]);
+
+	function addWord(word: string) {
+		words.push(word);
+	}
+</script>
+
+<ul>
+	{#each words as word}
+		<li>
+			{word}
+		</li>
+	{/each}
+</ul>
+
+<form
+	onsubmit={(e) => {
+		e.preventDefault();
+		addWord(word);
+		word = '';
+	}}
+>
+	<input type="text" bind:value={word} placeholder="ボカロ曲名を入力してください" />
+	<button type="submit">追加</button>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
+<script>
+	import Game from '$lib/Game.svelte';
+</script>
+
 <h1>ボカロ曲名しりとり！</h1>
 <p>「ボカロ曲名しりとり！」とは、ボカロ曲名を使ったしりとりゲームです。</p>
 
+<Game />
 
 <footer>
   <a href="https://mkpo.li/">
-  @mkpoli
+    @mkpoli
   </a>
 </footer>
