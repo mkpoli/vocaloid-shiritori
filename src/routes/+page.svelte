42c82b8bc2ab6c0d150e6672c0eefc52b95c2941
mkpoli <git@mkpo.li>
2024-09-29 13:24:33 +0900

    feat: add data feedback form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,21 @@
 </script>
 
 <main class="text-center py-12 flex flex-col gap-4">
-  <h1 class="text-2xl font-bold">ボカロ曲名しりとり！</h1>
-  <p class="text-lg">「ボカロ曲名しりとり！」とは、ボカロ曲名を使ったしりとりゲームです。</p>
-  
-  <Game vocaloids={data.vocaloids} />
+	<h1 class="text-2xl font-bold">ボカロ曲名しりとり！</h1>
+	<p class="text-lg">「ボカロ曲名しりとり！」とは、ボカロ曲名を使ったしりとりゲームです。</p>
+
+	<Game vocaloids={data.vocaloids} />
 </main>
 
 <footer class="text-center py-12">
-	<a href="https://mkpo.li/"> @mkpoli </a>
+	<p>
+		<a
+			href="https://github.com/blueset/vocaloid-yomigana/issues"
+			class="text-blue-600 hover:underline no-underline">楽曲情報修正・追加依頼</a
+		>
+	</p>
+
+	<p>
+		<a href="https://mkpo.li/"> @mkpoli </a>
+	</p>
 </footer>
