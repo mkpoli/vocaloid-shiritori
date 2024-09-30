eb06830a69818f68a53bff45307d0884bf2cb706
mkpoli <git@mkpo.li>
2024-09-30 12:06:18 +0900

    feat: add favicons and manifests

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,23 +4,6 @@
 
 <svelte:head>
 	<title>ボカロ曲名しりとり</title>
-	<meta name="description" content="ボカロの曲名でしりとりを楽しめるゲームです。" />
-	<meta name="keywords" content="ボカロ, 曲名, しりとり, ゲーム" />
-	<meta name="author" content="mkpoli" />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content="@mkpoli" />
-	<meta name="twitter:creator" content="@mkpoli" />
-	<meta name="twitter:title" content="ボカロ曲名しりとり" />
-	<meta name="twitter:description" content="ボカロ曲名しりとりを楽しめるゲームです。" />
-	<meta name="twitter:image" content="https://vocaloid-shiritori.mkpo.li/ogp.png" />
-	<meta property="og:title" content="ボカロ曲名しりとり" />
-	<meta property="og:description" content="ボカロの曲名でしりとりを楽しめるゲームです。" />
-	<meta property="og:image" content="https://vocaloid-shiritori.mkpo.li/ogp.png" />
-	<meta property="og:url" content="https://vocaloid-shiritori.mkpo.li" />
-	<meta property="og:type" content="website" />
-	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="630" />
-	<meta property="og:image:alt" content="ボカロ曲名しりとり" />
 </svelte:head>
 
 <slot />
