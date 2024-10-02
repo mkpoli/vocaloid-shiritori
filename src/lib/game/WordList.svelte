b7ae311931e003db401785010245b3ae74b69010
mkpoli <git@mkpo.li>
2024-10-03 00:55:35 +0900

    feat: display user name if not self in WordList

--- src/lib/game/WordList.svelte
+++ src/lib/game/WordList.svelte
@@ -2,6 +2,7 @@
 	import { indexNextChar } from '$lib/shiritori';
 	import type { Gamemode, Word } from '$lib/game';
 	import Thinking from '$lib/Thinking.svelte';
+	import { userManager } from '$lib/user.svelte';
 
 	let {
 		words,
@@ -19,17 +20,31 @@
 </script>
 
 <ul class="mx-auto flex w-full list-inside flex-col items-center justify-start gap-2 bg-white">
-	{#each words as {vocaloid, yomigana, sender}}
+	{#each words as { vocaloid, yomigana, sender }}
 		{@const index = indexNextChar(yomigana, stripChouon)}
+		{@const isMe = sender.type === 'user' && sender.username === userManager.username}
 		<li
-			class="w-max rounded-md px-3 py-2 shadow-sm"
-			class:self-start={gamemode !== 'single' && sender.type === 'computer'}
-			class:bg-blue-50={gamemode !== 'single' && sender.type === 'computer'}
-			class:self-end={gamemode !== 'single' && sender.type === 'user'}
-			class:bg-green-50={gamemode !== 'single' && sender.type === 'user'}
+			class="flex w-max flex-col rounded-md px-3 py-2 shadow-sm"
+			class:self-start={(gamemode === 'computer' && sender.type === 'computer') ||
+				(gamemode === 'public' && !isMe)}
+			class:bg-blue-50={(gamemode === 'computer' && sender.type === 'computer') ||
+				(gamemode === 'public' && !isMe)}
+			class:self-end={(gamemode === 'computer' && sender.type === 'user') ||
+				(gamemode === 'public' && isMe)}
+			class:bg-green-50={(gamemode === 'computer' && sender.type === 'user') ||
+				(gamemode === 'public' && isMe)}
 			class:self-center={gamemode === 'single'}
 			class:bg-gray-50={gamemode === 'single'}
+			class:justify-start={gamemode === 'public' && !isMe}
+			class:justify-end={gamemode === 'public' && isMe}
+			class:items-end={gamemode === 'public' && isMe}
+			class:items-start={gamemode === 'public' && !isMe}
 		>
+			{#if !isMe && sender.type === 'user'}
+				<span class="text-xs font-bold text-blue-400">
+					{sender.username}
+				</span>
+			{/if}
 			<ruby class="inline-flex gap-1 self-end">
 				{vocaloid}
 				<rt class="text-gray-400">
