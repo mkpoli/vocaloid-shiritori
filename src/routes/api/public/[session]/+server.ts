4fb21a69729a31f6cd33ce781f39db60683ad36e
mkpoli <git@mkpo.li>
2024-10-03 11:12:38 +0900

    feat: add req check in /api/public/[session]

--- src/routes/api/public/[session]/+server.ts
+++ src/routes/api/public/[session]/+server.ts
@@ -2,6 +2,7 @@ import { publicGameRecords } from '$lib/db/schema';
 import { error, type RequestHandler } from '@sveltejs/kit';
 import { drizzle } from 'drizzle-orm/d1';
 import { eq } from 'drizzle-orm';
+
 export const POST: RequestHandler = async ({ request, platform, params }) => {
 	try {
 		const DB = platform?.env?.DB;
@@ -17,11 +18,16 @@ export const POST: RequestHandler = async ({ request, platform, params }) => {
 			throw error(403, 'Invalid session');
 		}
 
-		const { vocaloid, username } = (await request.json()) as {
+		const req = await request.json();
+		if (typeof req !== 'object' || !req || !('vocaloid' in req) || !('username' in req)) {
+			throw error(403, 'Invalid request');
+		}
+
+		const { vocaloid, username } = req as {
 			vocaloid: string;
 			username: string;
 		};
-
+	
 		await db.insert(publicGameRecords).values({
 			songName: vocaloid,
 			username,
@@ -29,9 +35,9 @@ export const POST: RequestHandler = async ({ request, platform, params }) => {
 		});
 
 		return new Response(null, { status: 200 });
-	} catch (error) {
-		console.error(error);
-		return new Response(null, { status: 500 });
+	} catch (e) {
+		console.error(e);
+		return error(500, 'Failed to insert record');
 	}
 };
 
